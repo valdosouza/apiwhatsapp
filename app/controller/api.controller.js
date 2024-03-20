ecb75abfc6ad4beee230f0356a863b297c245d1f
Valdo Souza <devops@setes.com.br>
2024-03-20 10:27:45 -0300

    Adaptação Linux

--- app/controller/api.controller.js
+++ app/controller/api.controller.js
@@ -17,6 +17,20 @@ class ApiController extends Base {
                     venom
                         .create({
                             session: body.instance_name,
+                            puppeteer: {
+                                headless: false,
+                                args: [
+                                    '--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/65.0.3325.106 Safari/537.36',
+                                    '--no-sandbox',
+                                    '--disable-setuid-sandbox',
+                                    '--disable-dev-shm-usage',
+                                    '--disable-accelerated-2d-canvas',
+                                    '--no-first-run',
+                                    '--no-zygote',
+                                    '--single-process',
+                                    '--disable-gpu'
+                                ],
+                            }
                         },
                             (base64Qr, asciiQR, attempts, urlCode) => {
                                 console.log(asciiQR); // Optional to log the QR in the terminal
@@ -42,12 +56,50 @@ class ApiController extends Base {
                                 );
                             },
                             undefined,
-                            { logQR: false }
+                            {
+                                headless: true,
+                                logQR: true,
+                                browserWS: '', //browserless !=  '' ? browserless.replace('https://', 'wss://')+'?token='+token_browser : '',
+                                useChrome: true,
+                                updatesLog: true,
+                                autoClose: 90000,
+                                disableSpins: false,
+                                browserArgs: [
+                                    '--log-level=3',
+                                    '--no-default-browser-check',
+                                    '--disable-site-isolation-trials',
+                                    '--no-experiments',
+                                    '--ignore-gpu-blacklist',
+                                    '--ignore-certificate-errors',
+                                    '--ignore-certificate-errors-spki-list',
+                                    '--disable-gpu',
+                                    '--disable-extensions',
+                                    '--disable-default-apps',
+                                    '--enable-features=NetworkService',
+                                    '--disable-setuid-sandbox',
+                                    '--no-sandbox',
+                                    // Extras
+                                    '--disable-webgl',
+                                    '--disable-threaded-animation',
+                                    '--disable-threaded-scrolling',
+                                    '--disable-in-process-stack-traces',
+                                    '--disable-histogram-customizer',
+                                    '--disable-gl-extensions',
+                                    '--disable-composited-antialiasing',
+                                    '--disable-canvas-aa',
+                                    '--disable-3d-apis',
+                                    '--disable-accelerated-2d-canvas',
+                                    '--disable-accelerated-jpeg-decoding',
+                                    '--disable-accelerated-mjpeg-decode',
+                                    '--disable-app-list-dismiss-on-blur',
+                                    '--disable-accelerated-video-decode',
+                                ]
+                            },
+
                         )
                         .then(async (client) => {
                             BrowserInstances.init(body, client)
-                                .then((instance) => {
-                                    console.log(instance);
+                                .then((instance) => {                                    
                                     this.start(instance);
                                     resolve(instance);
                                 })
